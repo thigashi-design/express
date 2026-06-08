82d42b7a4c8045c71b7504ea2642f5b297524f3b
t.higashi <t.higashi@rise.okinawa>
2026-06-08 20:38:31 +0900

    second

--- myapp/routes/notes.js
+++ myapp/routes/notes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// 接続情報を設定
+// 接続情報を設定です
 const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://thigashi:tayu0615@cluster0.9kdiuut.mongodb.net/?appName=Cluster0";
 const client = new MongoClient(uri);
